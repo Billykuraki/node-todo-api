1a133abb706e7560ae54d9efce3996f8b2aaf0e8
Billy Hsieh <kuraki.dream@gmail.com>
2018-03-17 22:56:11 +0800

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id:10
-}
+var password = '123abc';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashPassword = '$2a$10$riufL0lrWMpTZzWnpguKL.LiwXWMz2.jwrIcBorcq63SdK.S4U8bG';
+
+bcrypt.compare(password, hashPassword, (err, res) => {
+    console.log(res);
+});
+
+// var data = {
+//     id:10
+// }
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);node 
 
 
 // var message = 'I am user numer 3';
